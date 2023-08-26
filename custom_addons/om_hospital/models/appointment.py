bf60b1a87e143a33e5189b2a6c813f602b71f5b2
BareketMuhammet <muhammetbareket95@gmail.com>
2023-08-26 22:06:44 +0300

    Add onchange function

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -9,4 +9,11 @@ class HospitalAppointment(models.Model):
 
     patient = fields.Many2one('hospital.patient', string='Patient')
     gender = fields.Selection(string='Gender', related="patient.gender", readonly=True)
+    ref = fields.Char(string='Reference')
     appointment_date = fields.Datetime(string='Appointment Date', default=datetime.datetime.now())
+
+    @api.onchange('patient')
+    def onchange_patient(self):
+        for rec in self:
+            rec.ref = rec.patient.ref
+
