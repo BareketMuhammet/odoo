87e20d2416bf5e918868676f27ce08b1c0991ef4
BareketMuhammet <muhammetbareket95@gmail.com>
2023-08-27 14:22:51 +0300

    Add buttons and rainbow effect and statusbar

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -13,8 +13,19 @@ class HospitalAppointment(models.Model):
     ref = fields.Char(string='Reference')
     appointment_date = fields.Datetime(string='Appointment Date', default=datetime.datetime.now())
     prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([('0', 'Normal'), ('1', 'Low'), ('2', 'High'), ('3', 'Very High')], string='Priority')
+    status = fields.Selection([('progress', 'In Progress'), ('done', 'Done'), ('canceled', 'Canceled')], string='Status', default='progress', required=True)
 
     @api.onchange('patient')
     def onchange_patient(self):
         for rec in self:
             rec.ref = rec.patient.ref
+
+    def object_action_test(self):
+        return {
+            'effect': {
+                'fadeout': 'slow',
+                'message': "Hii Rainbow",
+                'type': 'rainbow_man',
+            }
+        }
